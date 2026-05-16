c754c0a544efd95a1de871afb415cc5438ae0dda
desxl3te <likalse@bk.ru>
2026-05-16 20:31:49 +0300

    requests_scissors_bar
    запросы

--- requests_scissors_bar.sql
+++ requests_scissors_bar.sql
@@ -1,12 +1,15 @@
 USE scissors_bar;
 
 -- обычное меню 
-SELECT * FROM menu_items;
+SELECT * FROM menu_items WHERE is_secret = FALSE;
 
--- секретное меню
-SELECT * FROM secret_menu;
+-- секретное меню 
+SELECT * FROM menu_items WHERE is_secret = TRUE;
 
--- столики
+-- все столики
+SELECT * FROM tables;
+
+-- свободные столики на 19:00
 SELECT * FROM tables 
 WHERE id NOT IN (
     SELECT table_id FROM reservations 
@@ -14,15 +17,9 @@ WHERE id NOT IN (
       AND status = 'confirmed'
 );
 
--- просмотр бронирований
-SELECT * FROM reservations 
-WHERE user_id = 1 
-   OR (table_id = 3 AND reservation_time = '2025-04-15 20:00:00');
+-- бронирования пользователя с id = 1
+SELECT * FROM reservations WHERE user_id = 1;
 
--- тестовый пользователь
-INSERT INTO users (user_name, password_hash, email, phone) 
-VALUES ('test_user', '123123', 'test123@scissors.bar', '+79123456789');
-  
--- тестовая бронь
-INSERT INTO reservations (table_id, user_id, reservation_time, guests_count) 
-VALUES (1, 1, '2025-04-15 19:00:00', 2);
+-- проверка столика 3 на 20:00
+SELECT * FROM reservations 
+WHERE table_id = 3 AND reservation_time = '2025-04-15 20:00:00';
