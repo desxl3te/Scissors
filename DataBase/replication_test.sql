cbf3581976a508a9cde72e4b23ff56ca6926af73
pechegurovapolina7-pixel <pechegurovapolina7@gmail.com>
2026-05-25 23:58:16 +0300

    Add files via upload

--- DataBase/replication_test.sql
+++ DataBase/replication_test.sql
@@ -1,4 +1,4 @@
--- master
-INSERT INTO users (user_name, email) VALUES ('Boris', 'boris@mail.com');
--- slave
-SELECT * FROM users WHERE name = 'Boris';
+SELECT * FROM scissors_bar.categories;
+INSERT INTO scissors_bar.categories (name) VALUES ('Демо-репликация');
+SELECT * FROM scissors_bar.categories;
+-- должны быть видны изменения в slave (добавляться строка)
